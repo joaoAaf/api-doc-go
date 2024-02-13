ea448a7b5dafd680cd21f0c3ad2a41483dc54528
joaoAaf <joao.anderson.af@gmail.com>
2024-02-13 16:54:30 -0300

    inclusão da função getAlbums e endpoint /albums metodo GET

--- main.go
+++ main.go
@@ -1,4 +1,14 @@
+// Para criação de uma api:
+// Definição de um router, que vai receber a requisição e encaminhar para um controler;
+// Definição de uma função para cada controler, que vai processar a requisição e retornar a resposta;
+
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Album struct {
 	// A notação após a declaração da variavel,
@@ -7,4 +17,21 @@ type Album struct {
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
+}
+
+var albums = []Album{
+	{Id: 0, Title: "Exemple Title", Artist: "Exemple Artist", Price: 0.00},
+}
+
+// "gin.Context" carrega os detalhes da solicitação,
+// pode validar e serializar o JSON de uma solicitação e
+// retornar uma resporta em JSON tambem.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.Run(":8080")
 }
