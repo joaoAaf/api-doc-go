c00849acecdb87b5666c0d01ca0dbbd724647217
joaoAaf <joao.anderson.af@gmail.com>
2024-02-17 11:16:33 -0300

    inclusão da função postAlbums e endpoint /albums/:id metodo GET e criação função addId

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -26,11 +27,19 @@ type AlbumDTO struct {
 }
 
 var lastId int
-var albums = []Album{}
+var albums = []Album{
+	{Id: addId(), Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{Id: addId(), Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{Id: addId(), Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func addId() int {
+	lastId += 1
+	return lastId
+}
 
 func (a *Album) ConvertAlbum(albumDto AlbumDTO) {
-	lastId += 1
-	a.Id = lastId
+	a.Id = addId()
 	a.Title = albumDto.Title
 	a.Artist = albumDto.Artist
 	a.Price = albumDto.Price
@@ -41,6 +50,22 @@ func (a *Album) ConvertAlbum(albumDto AlbumDTO) {
 // retornar uma resporta em JSON tambem.
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func getAlbumById(c *gin.Context) {
+	// o "c.Param" retorna o parametro
+	// enviado pelo usuario por meio da url
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		panic(err)
+	}
+	for _, a := range albums {
+		if a.Id == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 func postAlbums(c *gin.Context) {
@@ -59,6 +84,7 @@ func postAlbums(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumById)
 	router.POST("/albums", postAlbums)
 	router.Run(":8080")
 }
